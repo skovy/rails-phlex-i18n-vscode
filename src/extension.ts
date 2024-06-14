9cc531ab494d22052c2e4fa0915576df9e020918
Spencer Miskoviak <smiskoviak@gmail.com>
2024-06-14 16:33:55 -0600

    add hovering

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,128 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
+// TODO: remove.
 import yaml from "js-yaml";
 import { exec } from "child_process";
 import { snakeCase } from "lodash";
+import YAML from "yaml";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('"rails-phlex-i18n" is active!');
 
-  let disposable = vscode.commands.registerCommand(
+  const extractToTranslationCommand = vscode.commands.registerCommand(
     "rails-phlex-i18n.extractToTranslation",
     extractToTranslation
   );
+  context.subscriptions.push(extractToTranslationCommand);
 
-  context.subscriptions.push(disposable);
+  const openTranslationCommand = vscode.commands.registerCommand(
+    "rails-phlex-i18n.openTranslation",
+    openTranslation
+  );
+  context.subscriptions.push(openTranslationCommand);
+
+  const translationHover = vscode.languages.registerHoverProvider("ruby", {
+    provideHover,
+  });
+  context.subscriptions.push(translationHover);
 }
 
+const provideHover: vscode.HoverProvider["provideHover"] = async (
+  document,
+  position
+) => {
+  // TODO: handle variables, e.g. `t(".#{key}")` & `t(".#{key}", var: value)`
+  const range = document.getWordRangeAtPosition(position, /\bt\(\"\.[^"]+\"\)/);
+  if (!range) return;
+
+  const text = document.getText(range);
+  const match = text.match(/t\(\"\.([^"]+)\"\)/);
+  if (!match) return;
+
+  const key = match[1];
+  if (!key) return;
+
+  const translationFilePath = getTranslationFilePath();
+  if (!translationFilePath) return;
+
+  const translationPath = await getComputedTranslationPathForCurrentEditor(
+    document,
+    key
+  );
+
+  const lineCounter = new YAML.LineCounter();
+  const parsed = YAML.parseDocument<YAML.YAMLMap<YAML.Scalar, YAML.YAMLMap>>(
+    fs.readFileSync(translationFilePath, "utf8"),
+    {
+      keepSourceTokens: true,
+      lineCounter,
+    }
+  );
+
+  const findNodeByPath = (
+    keys: string[],
+    value: YAML.YAMLMap<YAML.Scalar, YAML.YAMLMap | YAML.Scalar> | null
+  ): YAML.Pair<YAML.Scalar, YAML.Scalar> | undefined => {
+    const [first, ...rest] = keys;
+
+    const currentItem = value?.items.find((item) => item.key.value === first);
+
+    if (rest.length === 0) {
+      return currentItem as YAML.Pair<YAML.Scalar, YAML.Scalar>;
+    } else {
+      return findNodeByPath(
+        rest,
+        currentItem?.value as YAML.YAMLMap<YAML.Scalar, YAML.YAMLMap>
+      );
+    }
+  };
+
+  const node = findNodeByPath(translationPath, parsed.contents);
+  const offset = node?.key.srcToken?.offset;
+  const line = lineCounter.linePos(offset!);
+
+  const contents = new vscode.MarkdownString(
+    `**${
+      node?.value?.value
+    }** <br> [Open translation key](command:rails-phlex-i18n.openTranslation?${JSON.stringify(
+      [line.line - 1, line.col - 1]
+    )})`
+  );
+  contents.isTrusted = true;
+
+  return new vscode.Hover(contents, range);
+};
+
+const openTranslation = async (line: number, column: number) => {
+  const translationFilePath = getTranslationFilePath();
+  if (!translationFilePath) return;
+
+  await vscode.workspace
+    .openTextDocument(translationFilePath)
+    .then(async (doc) => {
+      const position = new vscode.Position(line, column);
+      const options: vscode.TextDocumentShowOptions = {
+        selection: new vscode.Range(position, position),
+        preview: false,
+      };
+      await vscode.window.showTextDocument(doc, options);
+    });
+};
+
 async function extractToTranslation() {
-  const editor = vscode.window.activeTextEditor;
-  if (!editor) {
-    vscode.window.showErrorMessage(
-      "No active text editor found. Please open a file and select text to extract."
-    );
-    return;
-  }
+  const editor = getActiveEditor();
+  if (!editor) return;
 
-  const workspacePath = vscode.workspace.workspaceFolders?.[0].uri.path;
-  if (!workspacePath) {
-    vscode.window.showErrorMessage(
-      "No workspace folder found. Please open a workspace and try again."
-    );
-    return;
-  }
+  const workspacePath = getWorkspacePath();
+  if (!workspacePath) return;
 
-  const translationFilePath = path.join(workspacePath, "config/locales/en.yml");
-  const translations = yaml.load(
-    fs.readFileSync(translationFilePath, "utf8")
+  const translationFilePath = getTranslationFilePath();
+  if (!translationFilePath) return;
+
+  const translations = YAML.parse(
+    fs.readFileSync(translationFilePath, "utf8"),
+    { keepSourceTokens: true }
   ) as Record<string, any>;
 
   const selections = editor.selections;
@@ -63,49 +150,10 @@ async function extractToTranslation() {
       return;
     }
 
-    const appControllersPath = path.join(workspacePath, "app/controllers");
-    const appViewsPath = path.join(workspacePath, "app/views");
-
-    // Get the relative path of the current file from the 'app/views/' directory
-    const filePath = document.uri.fsPath;
-
-    // Compute the key based on the relative path of the current file.
-    let computedPath;
-    if (filePath.startsWith(appControllersPath)) {
-      // Conntrollers.
-      const relativePath = path.parse(
-        path.relative(appControllersPath, filePath)
-      );
-
-      // TODO: can we infer this from LSP/AST?
-      const controllerAction = await vscode.window.showInputBox({
-        title: "What is the controller action name?",
-      });
-      if (!controllerAction) {
-        vscode.window.showErrorMessage(
-          "No controller action provided. Please provide a controller action."
-        );
-        return;
-      }
-
-      computedPath = path
-        .join(relativePath.dir, relativePath.name.replace("_controller", ""))
-        .split(path.sep)
-        .concat(controllerAction);
-    } else if (filePath.startsWith(appViewsPath)) {
-      // Phlex views.
-      const relativePath = path.parse(path.relative(appViewsPath, filePath));
-      computedPath = path
-        .join(relativePath.dir, relativePath.name)
-        .split(path.sep);
-    } else {
-      vscode.window.showErrorMessage(
-        "The current file is not in the 'app/views/' or 'app/controllers/' directory. Please open a file from the 'app/views/' or 'app/controllers/' directory."
-      );
-      return;
-    }
-
-    const translationPath = ["en", ...computedPath, ...key.split(".")];
+    const translationPath = await getComputedTranslationPathForCurrentEditor(
+      document,
+      key
+    );
 
     translationPath.reduce((acc, key, index) => {
       if (index === translationPath.length - 1) {
@@ -118,7 +166,7 @@ async function extractToTranslation() {
     }, translations);
 
     const translation = `t(".${key}")`;
-    editor.edit((editBuilder) => {
+    await editor.edit((editBuilder) => {
       editBuilder.replace(selection, translation);
     });
   }
@@ -135,5 +183,84 @@ async function extractToTranslation() {
     }
   });
 }
+
+// Helper functions.
+const getActiveEditor = () => {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    vscode.window.showErrorMessage(
+      "No active text editor found. Please open a file and select text to extract."
+    );
+  }
+  return editor;
+};
+
+const getWorkspacePath = () => {
+  const workspacePath = vscode.workspace.workspaceFolders?.[0].uri.path;
+  if (!workspacePath) {
+    vscode.window.showErrorMessage(
+      "No workspace folder found. Please open a workspace and try again."
+    );
+  }
+  return workspacePath;
+};
+
+const getTranslationFilePath = () => {
+  const workspacePath = getWorkspacePath();
+
+  if (workspacePath) return path.join(workspacePath, "config/locales/en.yml");
+};
+
+const getComputedTranslationPathForCurrentEditor = async (
+  document: vscode.TextDocument,
+  key: string
+) => {
+  const workspacePath = getWorkspacePath();
+  if (!workspacePath) return [];
+
+  const appControllersPath = path.join(workspacePath, "app/controllers");
+  const appViewsPath = path.join(workspacePath, "app/views");
+
+  // Get the relative path of the current file from the 'app/views/' directory
+  const filePath = document.uri.fsPath;
+
+  // Compute the key based on the relative path of the current file.
+  let computedPath;
+  if (filePath.startsWith(appControllersPath)) {
+    // Conntrollers.
+    const relativePath = path.parse(
+      path.relative(appControllersPath, filePath)
+    );
+
+    // TODO: can we infer this from LSP/AST?
+    const controllerAction = await vscode.window.showInputBox({
+      title: "What is the controller action name?",
+    });
+    if (!controllerAction) {
+      vscode.window.showErrorMessage(
+        "No controller action provided. Please provide a controller action."
+      );
+      return [];
+    }
+
+    computedPath = path
+      .join(relativePath.dir, relativePath.name.replace("_controller", ""))
+      .split(path.sep)
+      .concat(controllerAction);
+  } else if (filePath.startsWith(appViewsPath)) {
+    // Phlex views.
+    const relativePath = path.parse(path.relative(appViewsPath, filePath));
+    computedPath = path
+      .join(relativePath.dir, relativePath.name)
+      .split(path.sep);
+  } else {
+    vscode.window.showErrorMessage(
+      "The current file is not in the 'app/views/' or 'app/controllers/' directory. Please open a file from the 'app/views/' or 'app/controllers/' directory."
+    );
+    return [];
+  }
+
+  return ["en", ...computedPath, ...key.split(".")];
+};
 
 export function deactivate() {}
